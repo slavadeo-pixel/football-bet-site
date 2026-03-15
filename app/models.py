00a258b6c540b1ba0770e36918e95a7ea443b04e
slavadeo-pixel <slavadeo@gmail.com>
2026-03-15 21:31:01 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,16 +3,51 @@ import math
 def poisson(k, lam):
     return (lam ** k * math.exp(-lam)) / math.factorial(k)
 
-def match_probabilities(lambda_home, lambda_away, max_goals=5):
+
+def match_probabilities(home_xg, away_xg, max_goals=6):
+
     probs = {}
-    for i in range(0, max_goals+1):
-        for j in range(0, max_goals+1):
-            probs[f"{i}-{j}"] = poisson(i, lambda_home) * poisson(j, lambda_away)
+
+    for i in range(max_goals + 1):
+        for j in range(max_goals + 1):
+
+            probs[f"{i}-{j}"] = poisson(i, home_xg) * poisson(j, away_xg)
+
     return probs
 
-def fair_odds(prob):
-    return round(1 / prob, 2) if prob > 0 else 0
 
-def value_percentage(model_prob, bookie_odds):
-    bookie_prob = 1 / bookie_odds
-    return round((model_prob - bookie_prob) * 100, 2)
+def outcome_probabilities(score_probs):
+
+    home = 0
+    draw = 0
+    away = 0
+
+    for score, p in score_probs.items():
+
+        h, a = map(int, score.split("-"))
+
+        if h > a:
+            home += p
+
+        elif h == a:
+            draw += p
+
+        else:
+            away += p
+
+    return home, draw, away
+
+
+def fair_odds(prob):
+
+    if prob == 0:
+        return 0
+
+    return round(1 / prob, 2)
+
+
+def value_bet(probability, odds):
+
+    value = probability * odds - 1
+
+    return round(value * 100, 2)
